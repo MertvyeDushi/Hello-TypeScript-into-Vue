0f253146ee142c59b497eadef0f7416eefd533eb
MertvyeDushi <RomanTyurin@outlook.com>
2021-02-09 23:21:43 +0300

    Init commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    >
+
+    <HelloComponent msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { habrModule } from '@/store'
+
+@Component({
+  components: {
+    HelloComponent: () => import('@/components/HelloComponent.vue')
+  },
+  name: 'HomeView'
+})
+
+export default class HomeView extends Vue {
+
+  get computedTest() {
+    return habrModule.getters.greetingDefault
+  }
+
+}
+
+</script>
